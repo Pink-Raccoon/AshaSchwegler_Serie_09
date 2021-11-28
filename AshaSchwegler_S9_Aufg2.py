a2ec17aabc7828d2360a23a96c65e8d2c8924a48
Pink-Raccoon <schweglerasha@gmail.com>
2021-11-28 22:47:33 +0100

    aufg3

--- AshaSchwegler_S9_Aufg2.py
+++ AshaSchwegler_S9_Aufg2.py
@@ -9,15 +9,16 @@ import numpy as np
 
 def AshaSchwegler_S9_Aufg2(A,squiggle_A,b,squiggle_b): 
    cond_A = np.linalg.cond(A,np.inf)
-   squiggle_A_inf = np.linalg.norm((A-squiggle_A),np.inf)
+   squiggle_A_inf = np.linalg.norm((A+squiggle_A),np.inf)
    A_inf = np.linalg.norm(A,np.inf)
-   squiggle_b_inf = np.linalg.norm((b-squiggle_b),np.inf)
+   squiggle_b_inf = np.linalg.norm((b+squiggle_b),np.inf)
    b_inf = np.linalg.norm(b,np.inf) 
    if checkCond(A, squiggle_A) == True:
-      rel_Fehler = ((cond_A/(1-(cond_A*squiggle_A_inf/A_inf)))*((squiggle_A_inf/A_inf)+(squiggle_b_inf/b_inf)))
+      d_max = ((cond_A/(1-(cond_A*squiggle_A_inf/A_inf)))*((squiggle_A_inf/A_inf)+(squiggle_b_inf/b_inf)))
       result_ohneFehler = np.linalg.solve(A,b)
-      result_mitFehler = np.linalg.solve(squiggle_A,squiggle_b)
-      return rel_Fehler, result_ohneFehler, result_mitFehler
+      result_mitFehler = np.linalg.solve((A+squiggle_A),(b+squiggle_b))
+      d_obs = result_ohneFehler - result_mitFehler
+      return d_max, d_obs
    else: 
       return "NaN"
         
@@ -26,7 +27,7 @@ def AshaSchwegler_S9_Aufg2(A,squiggle_A,b,squiggle_b):
     
 def checkCond(A, squiggle_A):
     cond_A = np.linalg.cond(A,np.inf)
-    squiggle_A_inf = np.linalg.norm((A-squiggle_A),np.inf)
+    squiggle_A_inf = np.linalg.norm((A+squiggle_A),np.inf)
     A_inf = np.linalg.norm(A,np.inf)
     if cond_A * ((squiggle_A_inf)/A_inf):
         return True
--- AshaSchwegler_S9_Aufg3.py
+++ AshaSchwegler_S9_Aufg3.py
@@ -5,3 +5,17 @@ Created on Sun Nov 28 22:23:05 2021
 @author: Asha
 """
 
+import numpy as np
+from AshaSchwegler_S9_Aufg2 import AshaSchwegler_S9_Aufg2
+
+def AshaSchwegler_S9_Aufg3():
+    for i in range(1000):
+        A = np.random.rand(100,100)
+        b = np.random.rand(100,1)
+        squiggle_A = A + np.rand(100,100)/10^5
+        squiggle_b = b + np.rand(100,1)/10^5
+        d_max = AshaSchwegler_S9_Aufg2(A, squiggle_A, b, squiggle_b).d_max
+        d_obs = AshaSchwegler_S9_Aufg2(A, squiggle_A, b, squiggle_b).d_max.d_obs
+        verh = d_max/d_obs
+        return d_max, d_obs, verh
+        
